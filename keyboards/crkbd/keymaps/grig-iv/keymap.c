71ac9e154a1a0290d24f3ec3fb8e78e14dccc441
grig-iv <abstractgrig@gmail.com>
2024-04-18 18:48:21 +0300

    new layout

--- keyboards/crkbd/keymaps/grig-iv/keymap.c
+++ keyboards/crkbd/keymaps/grig-iv/keymap.c
@@ -15,22 +15,22 @@ enum custom_keycodes {
 
 
 // Thumb row mods
-#define TMB_LM LT(SYM, KC_BSPC)
-#define TMB_LR LT(NUM, KC_ESC)
-#define TMB_RL LT(FUN, KC_SPC)
-#define TMB_RM LT(NAV, KC_ENT)
+#define TMB_LL LT(FUN, KC_DEL)
+#define TMB_LM KC_SPC
+#define TMB_LR LT(NUM, KC_BSPC)
+#define TMB_RL LT(SYM, KC_ENT)
+#define TMB_RM LT(NAV, KC_ESC)
+#define TMB_RR ALGR_T(KC_TAB)
 
 // Base
-#define GUI_A LGUI_T(KC_A)
-#define ALT_R LALT_T(KC_R)
-#define SFT_S LSFT_T(KC_S)
-#define CTRL_T LCTL_T(KC_T)
-#define CTL_N RCTL_T(KC_N)
-#define SFT_E RSFT_T(KC_E)
-#define ALT_I LALT_T(KC_I)
-#define GUI_O RGUI_T(KC_O)
-
-#define OS_ALGR OSM(MOD_RALT)
+#define GUI_W LGUI_T(KC_W)
+#define ALT_Z LALT_T(KC_Z)
+#define SFT_F LSFT_T(KC_F)
+#define CTRL_D LCTL_T(KC_D)
+#define CTRL_H RCTL_T(KC_H)
+#define SFT_U RSFT_T(KC_U)
+#define ALT_SLSH LALT_T(KC_SLSH)
+#define GUI_Y RGUI_T(KC_Y)
 
 // Nav
 #define B_PREV C(KC_PGUP)
@@ -48,25 +48,25 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.        ,-----------------------------------------------------.
-      _______,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,             KC_J,    KC_L,    KC_U,    KC_Y, _______, _______,
-      _______,   GUI_A,   ALT_R,   SFT_S,  CTRL_T,    KC_G,             KC_M,   CTL_N,   SFT_E,   ALT_I,   GUI_O, _______,
-      _______,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,             KC_K,    KC_H, KC_COMM,  KC_DOT, OS_ALGR, _______,
-                                 _______,  TMB_LM,  TMB_LR,           TMB_RL,  TMB_RM,  _______
+      _______,    KC_Q,   GUI_W,   SFT_F,    KC_P,    KC_B,             KC_J,    KC_L,   SFT_U,   GUI_Y, _______, _______,
+      _______,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,             KC_M,    KC_N,    KC_E,    KC_I,    KC_O, _______,
+      _______,   ALT_Z,    KC_X,    KC_C,  CTRL_D,    KC_V,             KC_K,  CTRL_H, KC_COMM,  KC_DOT,ALT_SLSH, _______,
+                                  TMB_LL,  TMB_LM,  TMB_LR,           TMB_RL,  TMB_RM,  TMB_RR
    ),                        //`--------------------------'        `--------------------------'
 
   [SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.        ,-----------------------------------------------------.
-      _______,  KC_GRV, KC_LCBR, KC_RCBR, KC_TILD, KC_CIRC,          KC_HASH, KC_DLR,  KC_PERC,   KC_AT, KC_BSLS, _______,
+      _______,  KC_GRV, KC_LCBR, KC_RCBR, KC_TILD, KC_CIRC,          KC_HASH, KC_DLR,  KC_PERC,   KC_AT, _______, _______,
       _______, KC_DQUO, KC_LPRN, KC_RPRN, KC_SCLN, KC_AMPR,            KC_LT, KC_MINS, KC_PLUS,  KC_EQL,   KC_GT, _______,
-      _______, KC_QUOT, KC_LBRC, KC_RBRC, KC_COLN, KC_PIPE,          KC_ASTR, KC_UNDS, KC_EXLM, KC_QUES, KC_SLSH, _______,
+      _______, KC_QUOT, KC_LBRC, KC_RBRC, KC_COLN, KC_PIPE,          KC_ASTR, KC_UNDS, KC_EXLM, KC_QUES, KC_BSLS, _______,
                                  _______, _______, _______,          _______, _______, _______
    ),                        //`--------------------------'        `--------------------------'
 
   [NUM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.        ,-----------------------------------------------------.
-      _______, G(KC_Q),    KC_7,    KC_8,    KC_9,  KC_ENT,          XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
-      _______,    KC_0,    KC_4,    KC_5,    KC_6,  KC_DEL,            KC_LT, KC_MINS, KC_PLUS,   KC_EQL,   KC_GT, _______,
-      _______,  KC_TAB,    KC_1,    KC_2,    KC_3, C(KC_V),          KC_ASTR, KC_UNDS, KC_COMM,   KC_DOT, KC_BSLS, _______,
+      _______, _______,    KC_7,    KC_8,    KC_9,  KC_ESC,          XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
+      _______,    KC_0,    KC_4,    KC_5,    KC_6,  KC_ENT,            KC_LT, KC_MINS, KC_PLUS,   KC_EQL,   KC_GT, _______,
+      _______, _______,    KC_1,    KC_2,    KC_3, C(KC_V),          KC_ASTR, KC_UNDS, KC_COMM,   KC_DOT, KC_BSLS, _______,
                                  _______, _______, _______,          _______, _______, _______
    ),                        //`--------------------------'        `--------------------------'
 
@@ -95,13 +95,11 @@ bool process_detected_host_os_kb(os_variant_t detected_os) {
         return false;
     }
     switch (detected_os) {
-        case OS_WINDOWS:
-            is_linux_system = false;
-            break;
         case OS_LINUX:
             is_linux_system = true;
             break;
         default:
+            is_linux_system = false;
             break;
     }
 
@@ -140,9 +138,12 @@ combo_t key_combos[] = {
 // PER KEY SETTINGS
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case TMB_LM:
+        case TMB_LL:
+        // case TMB_LM:
         case TMB_LR:
         case TMB_RL:
+        case TMB_RM:
+        case TMB_RR:
             return true;
         default:
             return false;
@@ -151,7 +152,12 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case TMB_RM:
+        // case TMB_LL:
+        case TMB_LM:
+        // case TMB_LR:
+        // case TMB_RL:
+        // case TMB_RM:
+        // case TMB_RR:
             return true;
         default:
             return false;
@@ -160,9 +166,12 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case GUI_O:
-        case GUI_A:
+        case GUI_W:
+        case GUI_Y:
             return TAPPING_TERM + 50;
+        case SFT_F:
+        case SFT_U:
+            return TAPPING_TERM + 30;
         default:
             return TAPPING_TERM;
     }
